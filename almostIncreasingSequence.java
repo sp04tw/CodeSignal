dce2954548f1f0e8d75020af1efad27350c9ae11
SP <30589096+sp04tw@users.noreply.github.com>
2022-02-02 14:37:56 -0500

    Update almostIncreasingSequence.java

--- almostIncreasingSequence.java
+++ almostIncreasingSequence.java
@@ -1,4 +1,9 @@
 boolean solution(int[] sequence) {
+    /*[1, 3, 2, 1] output:false
+    [1, 3, 2] out:true
+    [1, 2, 1, 2] out:false
+    [3, 6, 5, 8, 10, 20, 15] out:false
+    [3, 5, 67, 98, 3] out:true*/
     if(sequence.length==2) return true;
     int count=0;
     for(int i=0;i<sequence.length-1;i++){
